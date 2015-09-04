a09e478b2b25634854f32def0f44aedc56ee327d
Jim Quitevis <jim.quitevis@gmail.com>
2015-09-04 00:31:04 -0700

    Restrucutred tests + added log4j and other necessary libraries

--- testdatagenerator/src/main/java/com/jjdevbros/castellan/testdatagenerator/App.java
+++ testdatagenerator/src/main/java/com/jjdevbros/castellan/testdatagenerator/App.java
@@ -1,11 +1,15 @@
 package com.jjdevbros.castellan.testdatagenerator;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App {
     public static void main(String[] args) {
+        log.info("hey!");
         System.out.println("Hello World!");
     }
 }
---
+++ testdatagenerator/src/main/java/com/jjdevbros/castellan/testdatagenerator/Generator.java
@@ -0,0 +1,7 @@
+package com.jjdevbros.castellan.testdatagenerator;
+
+/**
+ * Created by lordbritishix on 04/09/15.
+ */
+public class Generator {
+}
